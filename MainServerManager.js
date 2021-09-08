8a9cd639afac86c0ebd018a9c7f2eef79fd01ea5
expiredy101 <yartaoley2005@mail.ru>
2021-09-09 00:22:38 +0300

    Some node js understanding and trying to do anything not stupid

--- MainServerManager.js
+++ MainServerManager.js
@@ -1,19 +1,36 @@
+// node js constants caller
 const express = require('express');
-const path = require('path');
 const webApp = express();
-const lobby_page_path = ".\\frontend\\markups\\menu\\lobby_page.html";
 const fileSystem = require("fs");
 
-var https = require('https');
+// paths config
+const LOBBY_PAGE_PATH = ".\\frontend\\markups\\menu\\lobby_page.html";
+const MAIN_GAME_TABLE_PATH = ".\\frontend\\markups\\game\\game_field.html";
 
-webApp.get("/", function(response, request){ 
-    fileSystem.readFile(lobby_page_path, function(error, html_page) {
+// css classes' names
+
+webApp.get("/", function(request, response){ 
+    fileSystem.readFile(LOBBY_PAGE_PATH, function(error, html_page) {
         if (error) {
             console.log(error)
-            response.status(500).send("Sorry we sucked some milks");
+            response.status(500).send("<h1>Sorry we sucked some milks</h1>");
         }
-        response.send("<h1>" + html_page + "</h1>");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(html_page);
+        response.end();
     })
 });
+
+webApp.get("/game", function(request, response) {
+    fileSystem.readFile(MAIN_GAME_TABLE_PATH, function(error, html_page) {
+        if (error) {
+            console.log(error);
+            response.status(500).send("<h1>Sorry we sucked some milks</h1>");
+        }
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(html_page);
+        response.end();
+    })
+})
 
 webApp.listen(process.env.PORT || 3000, () => console.log("listening on port http://localhost:3000"));
