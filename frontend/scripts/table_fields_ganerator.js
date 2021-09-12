5df81ec40f88641e590e4c29df7c34e91afa8065
expiredy101 <yartaoley2005@mail.ru>
2021-09-12 18:41:12 +0300

    Don't ask why

--- frontend/scripts/table_fields_ganerator.js
+++ frontend/scripts/table_fields_ganerator.js
@@ -1,11 +1,8 @@
 const TOTAL_GAME_TABLE_SIDES_COUNT = 4;
 const TOTAL_FIELD_ON_SIDE_COUNT = 8;
 
-const MAIN_FIELD_STYLE_CLASS_NAME = "someCssClassName"
-
 // css classes' names
 const STANDARD_LINE_FIELD_STYLE_CLASS = "game-table-field-card";
-const STANDARD_SIDE_STYLE_CLASS = "game-table-side";
 const STANDARD_FIELD_TEXT_STYLE_CLASS = "game-table-field-text";
 
 // html elements ids
@@ -14,8 +11,6 @@ const TABLE_FIELDS_BODY_DIV_ID = "game-table-field";
 const gameTableDiv = document.getElementById(TABLE_FIELDS_BODY_DIV_ID)
 
 for (var sideId = 0; sideId < TOTAL_GAME_TABLE_SIDES_COUNT; ++sideId) {
-    const gameTableSideDiv = document.createElement('div');
-    gameTableSideDiv.className = STANDARD_SIDE_STYLE_CLASS;
     for (var fieldOnSideId = 0; fieldOnSideId < TOTAL_FIELD_ON_SIDE_COUNT; ++fieldOnSideId) {
         const fieldUnderlineDiv = document.createElement('div');
         fieldUnderlineDiv.className = STANDARD_LINE_FIELD_STYLE_CLASS;
@@ -25,7 +20,6 @@ for (var sideId = 0; sideId < TOTAL_GAME_TABLE_SIDES_COUNT; ++sideId) {
         FieldIconDiv.className = STANDARD_FIELD_TEXT_STYLE_CLASS;
         
         fieldUnderlineDiv.append(FieldIconDiv);
-        gameTableSideDiv.append(fieldUnderlineDiv);
+        gameTableDiv.append(fieldUnderlineDiv);
     }
-    gameTableDiv.append(gameTableSideDiv);
 }
