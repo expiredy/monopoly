8f5cbed934f7bf170c059c998f5c75eb99cd9a3e
Ярослав <62770020+expiredy101@users.noreply.github.com>
2021-11-08 01:07:45 +0300

    Frontend (#2)
    * Rebuilding frontend in react js and renaming it for project name
    
    * Restruckturing default markup and building app logic
    
    * adding cards thing and other stuff
    
    * Adding new system of properties for element and trying to create backend
    
    Co-authored-by: expiredy101 <yartaoley2005@mail.ru>

---
+++ wonopoly-site/src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import GameField from "./../pages/GameField/GameField";
+
+function App() {
+  return (
+      <GameField sessionId={1}/>
+  );
+}
+
+export default App;
---
+++ wonopoly-site/src/pages/GameField/GameField.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "./GameFieldStyle.css"
+
+
+function GameField(properties: {sessionId: number}) {
+    return (
+        <div className="field-container">
+            
+        </div>
+    )
+
+}
+
+export default GameField;
