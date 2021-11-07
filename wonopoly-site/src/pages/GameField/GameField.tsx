f45b48bbf835e592e796b7113833a3126b68f1e2
expiredy101 <yartaoley2005@mail.ru>
2021-11-07 21:43:05 +0300

    adding cards thing and other stuff

--- wonopoly-site/src/pages/GameField/GameField.tsx
+++ wonopoly-site/src/pages/GameField/GameField.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import "./GameFieldStyle.css"
 
-function GameField(){
+
+function GameField(properties: {playersIdList: [],}) {
     return (
-        <div className="field-container">
-
+        <div className="field-container" id={properties.id}>
+            
         </div>
     )
 
