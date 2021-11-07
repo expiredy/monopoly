40738b2ae364ac8f44d8a1da6abc6c5e28df77db
expiredy101 <yartaoley2005@mail.ru>
2021-11-08 01:05:11 +0300

    still have no idea which utilit i can chose for backend and getting started with markaps

--- wonopoly-site/src/pages/GameField/components/GameTableBuilder.tsx
+++ wonopoly-site/src/pages/GameField/components/GameTableBuilder.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+function GameFieldGenerator(){
+    return (
+        <div></div>
+    );
+}
+
+
+function PlayersSectionGenerator(){
+    return (<div></div>);
+}
+
 function GameTableBuilder(){
     return(
         <div className="game-table">
+
             <div className="chat-container">
             
             </div>
+            {GameFieldGenerator()}
+            {PlayersSectionGenerator()}
         </div>
     );
 }
