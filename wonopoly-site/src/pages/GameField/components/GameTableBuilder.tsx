f40aa383b9b6fdeb4ca821bf4aa10202755d5328
Ярослав <62770020+expiredy101@users.noreply.github.com>
2021-11-08 23:05:11 +0300

    Frontend (#3)
    * Rebuilding frontend in react js and renaming it for project name
    
    * Restruckturing default markup and building app logic
    
    * adding cards thing and other stuff
    
    * Adding new system of properties for element and trying to create backend
    
    * still have no idea which utilit i can chose for backend and getting started with markaps
    
    * So i finally decide to use rust for backend in my project
    
    Co-authored-by: expiredy101 <yartaoley2005@mail.ru>

--- wonopoly-site/src/pages/GameField/components/GameTableBuilder.tsx
+++ wonopoly-site/src/pages/GameField/components/GameTableBuilder.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
+
+function GameFieldGenerator(){
+    var gameFieldConstructor: [] = [];
+    return (
+        <div>{gameFieldConstructor}</div>
+    );
+}
+
+
+function PlayersSectionGenerator(){
+    var mainConstructor: [] = [];
+    return(<div>{mainConstructor}</div>);
+}
+
 function GameTableBuilder(){
     return(
         <div className="game-table">
+
             <div className="chat-container">
             
             </div>
+            {GameFieldGenerator()}
+            {PlayersSectionGenerator()}
+
+
         </div>
     );
 }
