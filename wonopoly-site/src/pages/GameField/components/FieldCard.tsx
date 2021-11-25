53b48a6a0e985105a29c65dca2e7bb45e54244e3
expiredy101 <yartaoley2005@mail.ru>
2021-11-25 23:27:04 +0300

    Starting implomenting base markups and write backend server

--- wonopoly-site/src/pages/GameField/components/FieldCard.tsx
+++ wonopoly-site/src/pages/GameField/components/FieldCard.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function FieldCard(properties: {id: string;}) {
-
+    return(
+        <div className="field-card">
+            
+        </div>
+    );
 }
 
 export default FieldCard;
