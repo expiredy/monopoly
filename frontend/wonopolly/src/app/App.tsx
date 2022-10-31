14059b4af3e20ba6c33c0cc1d5e0678158b958ac
expiredy101 <yaraoley2005@mail.ru>
2022-10-31 10:27:42 +0300

    Adding default structure and react-routing-dom

---
+++ frontend/wonopolly/src/app/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Authentication from '../pages/auntification/Authentication';
+
+import './App.css';
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Authentication />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/wonopolly/src/pages/game-field/GameField.tsx
@@ -0,0 +1,18 @@
+import { Component, ReactNode } from "react"
+
+export default class GameField extends Component{
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            ws: null
+        };
+    }
+
+    render(){ 
+        return (
+            <h1>A</h1>
+        )
+    }
+}
